b0af5b088d669f91217ec01c9e4e81e1c23b25c5
Yarik <you@example.comyarikvitovsky@gmail.com>
2025-11-04 06:25:39 +0200

    Remove star indicator from Cat Meme Generator title

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const projects = [
     },
     {
         id: 3,
-        title: "Cat Meme Generator ⭐",
+        title: "Cat Meme Generator",
         description: "Fun web app that generates hilarious cat jokes with random cat images using public APIs",
         link: "https://hilarious-banoffee-1e3d7a.netlify.app/",
         isLive: true,
